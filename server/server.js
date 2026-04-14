1c4296195de275f2a22e02f0a374e4f3ffc7f643
sandeshdubeyy <sandeshdubeyy@gmail.com>
2026-04-15 00:51:58 +0530

    feat: add Clerk and Inngest integration to server
    - Added @clerk/express as a dependency for authentication middleware.
    - Integrated clerkMiddleware into the Express app for user authentication.
    - Added inngest as a dependency and set up API routes for Inngest functions.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.configs.js'
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
-const app = express()
+const app = express(    )
 const port=3000
 
 
@@ -12,10 +15,11 @@ await connectDB();
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 //api routes
 app.get('/',(req,res)=> res.send('Server Is Live!'))
-
+app.use('/api/inngest',serve({ client: inngest, functions }))
 
 app.listen(port,()=> console.log(`Server listening at http://localhost:${port}`))
 
