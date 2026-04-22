9482ab6ada2f45f041eb1f6d55ddc839972c0c68
sandeshdubeyy <sandeshdubeyy@gmail.com>
2026-04-22 14:39:31 +0530

    feat: enhance movie pages with API integration and user context for dynamic data rendering

--- client/src/pages/MovieDetails.pages.jsx
+++ client/src/pages/MovieDetails.pages.jsx
@@ -7,20 +7,42 @@ import timeFormat from '../lib/timeFormat';
 import DateSelect from '../components/DateSelect';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
+import { useAppContext } from '../context/AppContext';
+import toast from 'react-hot-toast';
 
 const MovieDetails = () => {
+  const {shows, axios, getToken, user, favoriteMovies, fetchFavoriteMovies, image_base_url}= useAppContext()
 
   const navigate = useNavigate();
   const {id} = useParams();
   const [show,setShow] = useState(null);
 
   const getShow = async () => {
-    const show = dummyShowsData.find(show => show._id === id)
-    if(show){
-      setShow({
-      movie:show,
-      dateTime:dummyDateTimeData
-    })}
+    try {
+      const { data } = await axios.get(`/api/show/${id}`)
+      if(data.success){
+        setShow(data)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleFavorite = async () => {
+    try {
+      if(!user){
+        return toast.error("Please login to proceed")
+      }
+      const {data} = await axios.post('/api/user/update-favorite',{movieId: id},{headers:{
+              Authorization: `Bearer ${await getToken()}`
+          }})
+          if(data.success){
+            await fetchFavoriteMovies()
+            toast.success(data.message)
+          }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(()=>{
@@ -30,7 +52,7 @@ const MovieDetails = () => {
   return ( show ? (
     <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
       <div className='flex flex-row m:flex-row gap-8 max-w-6xl mx-auto'>
-        <img src={show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
+        <img src={image_base_url+show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
         <div className='relative flex flex-col gap-3'>
           <BlurCircle top='-100px' left='-100px'/>
           <p className='text-primary'>ENGLISH</p>
@@ -43,7 +65,7 @@ const MovieDetails = () => {
           <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl '>{show.movie.overview}</p>
 
           <p>
-            {timeFormat(show.movie.runtime)} - {show.movie.genres.map(genre => genre.name).join(", ")} - {new Date(show.movie.release_date).toLocaleDateString("en-GB", {
+            {timeFormat(show.movie.runtime)} - {show.movie.genre.map(genre => genre.name).join(", ")} - {new Date(show.movie.release_date).toLocaleDateString("en-GB", {
               day: "numeric",
               month: "long",
               year: "numeric"
@@ -56,8 +78,8 @@ const MovieDetails = () => {
               Watch Trailer  
             </button>
             <a href='#dateSelect' className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>Buy Tickets</a>
-            <button className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
-              <Heart className='w-5 h-5'/>
+            <button onClick={handleFavorite} className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
+              <Heart className={`w-5 h-5 ${favoriteMovies.find(movie => movie._id) ? 'fill-primary text-primary' : ""}`}/>
             </button>
           </div>
         </div>
@@ -68,7 +90,7 @@ const MovieDetails = () => {
         <div className='flex items-center gap-4 w-max px-4'>
           {show.movie.casts.slice(0.12).map((cast,index)=>(
             <div key={index} className='flex flex-col items-center text-center'>
-              <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
+              <img src={image_base_url+cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
               <p className='font-medium text-xs mt-3'>{cast.name}</p>
             </div>
           ))}
@@ -78,7 +100,7 @@ const MovieDetails = () => {
 
       <p className='text-lg font-medium mt-20 mb-8'>You May Also Like</p>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
-          {dummyShowsData.slice(0,4).map((movie,index)=>(
+          {shows.slice(0,4).map((movie,index)=>(
             <MovieCard key={index} movie={movie}/>
           ))}
       </div>
--- client/src/pages/SeatLayout.pages.jsx
+++ client/src/pages/SeatLayout.pages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { assets, dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
@@ -6,31 +6,81 @@ import { ArrowRightIcon, ClockIcon, TicketCheck } from 'lucide-react'
 import isoTimeFormat from '../lib/isoTimeFormat'
 import BlurCircle from '../components/BlurCircle'
 import toast from "react-hot-toast";
+import { useAppContext } from '../context/AppContext'
 
 const SeatLayout = () => {
-
+  const {axios, getToken, user}= useAppContext()
+  
   const groupRows = [["A","B"],["C","D"],["E","F"],["G","H"],["I","J"]]
 
   const {id,date} = useParams()
   const [selectedSeats, setSelectedSeats] = useState([])
   const [selectedTime, setSelectedTime] = useState(null)
   const [show, setShow] = useState(null)
+  const [occupiedSeats,setOccupiedSeats]=useState([])
 
   const navigate = useNavigate()
   
   const getShow =  async () => {
-    const show = dummyShowsData.find(show => show._id === id)
-    if(show){
-      setShow({
-        movie:show,
-        dateTime:dummyDateTimeData
-      })
+    try {
+      const { data } = await axios.get(`/api/show/${id}`,{headers:{
+                Authorization: `Bearer ${await getToken()}`
+            }})
+            if(data.success){
+              setShow(data)
+            }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const getOccupiedSeats = async () => {
+    try {
+      const { data } = await axios.get(`/api/booking/seats/${selectedTime.showId}`)
+      if(data.success){
+        setOccupiedSeats(data.occupiedSeats)
+      }
+      else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const bookTickets = async () => {
+    try {
+      if(!user){
+        return toast.error('Please login to proceed')
+      }
+      if(!selectedTime || !selectedSeats.length){
+        return toast.error("Please select time and seat")
+      }
+
+      const { data } = await axios.post('/api/booking/create',{showId: selectedTime.showId, selectedSeats},{headers:{
+                Authorization: `Bearer ${await getToken()}`
+            }})
+
+            if(data.success){
+              toast.success(data.message)
+              navigate('/my-booking')
+            }
+            else{
+              toast.error(data.message)
+            }
+    } catch (error) {
+      toast.error(error.message)
     }
   }
 
   useEffect(()=>{
     getShow()
   },[])
+
+  useEffect(()=>{
+    if(selectedTime){
+      getOccupiedSeats()
+    }
+  },[selectedTime])
 
   const handleSeatClick = (seatId) => {
     if(!selectedTime) {
@@ -41,6 +91,10 @@ const SeatLayout = () => {
       return toast("You can only select 5 seats")
     }
     
+    if(occupiedSeats.includes(seatId)){
+      return toast('This seat is already booked')
+    }
+
     setSelectedSeats(prev=>prev.includes(seatId) ? prev.filter(seat => seat !== seatId) : [...prev, seatId])
   }
 
@@ -50,7 +104,8 @@ const SeatLayout = () => {
         {Array.from({length:count},(_,i) => {
             const seatId = `${row}${i+1}`;
             return (
-              <button key={seatId} onClick={() => handleSeatClick(seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && "bg-primary text-white"}`}>
+              <button key={seatId} onClick={() => handleSeatClick(seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer 
+              ${selectedSeats.includes(seatId) && "bg-primary text-white"} ${occupiedSeats.includes(seatId) && 'opacity-50'}`}>
                 {seatId}
               </button>
             )
@@ -96,7 +151,7 @@ const SeatLayout = () => {
             </div>
           </div>
 
-          <button onClick={()=> navigate('/my-bookings')} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95'>
+          <button onClick={bookTickets} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95'>
               Proceed to Checkout
               <ArrowRightIcon strokeWidth={3} className='w-4 h-4' />
           </button>
